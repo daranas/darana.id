fa1b71df2c8435881ecf553ec3c1039c1a0615a5
Darana Vidya <darana.sv@gmail.com>
2020-04-04 04:06:39 +0700

    remove unused code

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Layout from '../components/layout'
+import Head from 'next/head'
 
 export default function Index () {
   return (
     <Layout>
+      <Head>
+        <title>DaranaSV - Freelance Front End Developer, Web Programmer Based in Bandung, Indonesia</title>
+      </Head>
       <h1>Ini Home</h1>
     </Layout>
   )
